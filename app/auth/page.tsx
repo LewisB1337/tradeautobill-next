66b17681b95c12aff34ee6294a6d87373ebb0149
LewisB1337 <lewisblackwork1@gmail.com>
2025-08-06 00:34:32 +0100

    Fix app/auth/page.tsx: add default export component

---
+++ app/auth/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function AuthPage() {
+  return (
+    <div>
+      <h1>Authentication</h1>
+      <p>Click below to sign in:</p>
+      {/* your sign-in button here */}
+    </div>
+  )
+}
