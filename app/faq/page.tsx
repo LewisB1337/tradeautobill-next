4141e1e7c344f8172a9960bb7a0b9ce3a33d538f
LewisB1337 <lewisblackwork1@gmail.com>
2025-08-05 03:52:40 +0100

    Make all pages Client Components / add Suspense

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -1,17 +1,1 @@
-'use client';
-export const dynamic = 'force-dynamic';
-
-
-// app/faq/page.tsx
-
-import { useRouter } from 'next/navigation';
-
-export default function Page() {
-  return (
-    <section className="container py-10">
-      <h1>Help & FAQ</h1>
-
-      <p>Content goes here.</p>
-    </section>
-  );
-}
+'use client';export const dynamic = 'force-dynamic';// app/faq/page.tsximport { useRouter } from 'next/navigation';export default function Page() {  return (    <section className="container py-10">      <h1>Help & FAQ</h1>      <p>Content goes here.</p>    </section>  );}
--- app/terms/page.tsx
+++ app/terms/page.tsx
@@ -1,17 +1,1 @@
-'use client';
-export const dynamic = 'force-dynamic';
-
-
-
-// app/create/page.tsx
-
-import { useRouter } from 'next/navigation'
-export default function Page(){
-  return (
-    <section className="container py-10">
-      <h1>Terms of Service</h1>
-      <p className='muted'>Last updated: 2 August 2025</p>
-      <p>Content goes here.</p>
-    </section>
-  );
-}
+'use client';export const dynamic = 'force-dynamic';// app/create/page.tsximport { useRouter } from 'next/navigation'export default function Page(){  return (    <section className="container py-10">      <h1>Terms of Service</h1>      <p className='muted'>Last updated: 2 August 2025</p>      <p>Content goes here.</p>    </section>  );}
